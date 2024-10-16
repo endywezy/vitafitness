c41673d8248b181e5052a1bd64240411f5b63fd0
endywezy <endywezy@gmail.com>
2024-10-16 19:10:01 +0100

    exercise model to remove unnecessary comment

--- models/exercise.js
+++ models/exercise.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const exerciseSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   type: { type: String, required: true },
-  duration: { type: Number, required: true }, // in minutes
+  duration: { type: Number, required: true },
   date: { type: Date, default: Date.now }
 });
 
